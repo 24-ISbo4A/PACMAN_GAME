e92f2c67e0c443119e65b5a921c8e43346bc2d7b
sun1zu <kiri.andreev2006@yandex.ru>
2025-03-21 00:46:20 +0300

    fixed links to deleted pngs and gifs closed #6

--- obj/Debug/net8.0-windows/PACMAN_GAME.AssemblyInfo.cs
+++ obj/Debug/net8.0-windows/PACMAN_GAME.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PACMAN_GAME")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+abff5b5c3527f61ddbb73e6a0be01935527a2c65")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+aee6a054cc5de6e1ffdffedb09cce31d36d644d6")]
 [assembly: System.Reflection.AssemblyProductAttribute("PACMAN_GAME")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PACMAN_GAME")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
